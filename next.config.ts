e25e6acafc33a3fbc78eb2875d1f846eb319b05e
pokerty <uglypo@gmail.com>
2024-12-05 21:07:31 +0800

    added check for prod in next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/personal-website' : '',
+  assetPrefix: isProd ? '/personal-website/' : '',
   images: {
     remotePatterns: [
       {
@@ -31,27 +35,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'pbs.twimg.com',
         port: '',
-        pathname: '/**', 
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '/**', 
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.prod.website-files.com',
-        port: '',
-        pathname: '/**', 
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.squarespace-cdn.com',
-        port: '',
-        pathname: '/**', 
+        pathname: '/**',
       },
     ],
+    unoptimized: true,
   },
 };
 
