98dc0606f34deb168d4a3576b80fd455079a18a9
pokerty <uglypo@gmail.com>
2025-01-27 17:06:47 +0800

    fixed spacing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
             />
             </div>
 
-            <div className="flex justify-center relative my-52 z-10">
+            <div className="flex justify-center relative my-36 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <p className="uppercase tracking-widest text-xs text-center dark:text-blue-100 text-black-100 max-w-80">
                 Welcome to my portfolio
